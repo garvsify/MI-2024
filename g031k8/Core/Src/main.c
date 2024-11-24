eee60a12f4e3d0abd1733cd75a7833cd5aef0988
DESKTOP-L4887SG\James Garvey <jamesgarvey@me.com>
2024-11-24 20:05:16 +0000

    Re-read the HAL drivers and it said to use certain functions to start the timers depending on their modes so have added these

--- g031k8/Core/Src/main.c
+++ g031k8/Core/Src/main.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+	__disable_irq(); //disable interrupts
 	System_Init();
 
 	//DISABLE TIMERS and PWM OUTPUT
@@ -16,16 +17,21 @@ int main(void)
 	//START ADC TRIG. TIMER
 	__HAL_TIM_SET_COUNTER(&htim17, 0); //set counter to 0
 	__HAL_TIM_ENABLE(&htim17); //enable TIM17
+	HAL_TIM_OC_Start(&htim17); //enable ADC triggering
 
 	//WAIT
 	while(initial_ADC_conversion_complete == 0){}; //wait while first ADC conversion is ongoing
 
+	process_TIM16_raw_start_value_and_prescaler();
+	process_TIM16_final_start_value_and_prescaler_adjust();
+
 	//START FREQ. GEN and PWM GEN TIMERS and ENABLE PWM OUTPUT
 	__HAL_TIM_SET_COUNTER(&htim14, 0); //set TIM14 counter to 0
-	__HAL_TIM_ENABLE(&htim14); //enable TIM14
 	__HAL_TIM_SET_COUNTER(&htim14, 0); //set TIM16 counter to 0
+	__HAL_TIM_ENABLE(&htim14); //enable TIM14
 	__HAL_TIM_ENABLE(&htim16); //enable TIM16
-	__HAL_TIM_MOE_ENABLE(&htim14); //enable main output of TIM14 - i.e. PWM output ON
+	HAL_TIM_OC_Start(&htim16); //start freq. gen
+	HAL_TIM_PWM_Start(&htim14, TIM_CHANNEL_1); //start PWM
 
 	while (1)
 	{
