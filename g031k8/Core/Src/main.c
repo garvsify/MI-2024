0da2b312311dc79f33a18f904046e23935330e18
DESKTOP-L4887SG\James Garvey <jamesgarvey@me.com>
2024-11-24 19:31:44 +0000

    Prettied up main

--- g031k8/Core/Src/main.c
+++ g031k8/Core/Src/main.c
@@ -5,17 +5,22 @@ int main(void)
 {
 	System_Init();
 
+	//DISABLE TIMERS and PWM OUTPUT
 	__HAL_TIM_DISABLE(&htim14); //enable TIM14
 	__HAL_TIM_DISABLE(&htim16); //enable TIM16
 	__HAL_TIM_DISABLE(&htim17); //enable TIM17
-	__HAL_TIM_MOE_DISABLE(&htim14); //enable main output of TIM14 - i.e. PWM output OFF
+	__HAL_TIM_MOE_DISABLE(&htim14); //disable main output of TIM14 - i.e. PWM output OFF
 
 	__enable_irq(); //enable interrupts
+
+	//START ADC TRIG. TIMER
 	__HAL_TIM_SET_COUNTER(&htim17, 0); //set counter to 0
 	__HAL_TIM_ENABLE(&htim17); //enable TIM17
 
+	//WAIT
 	while(initial_ADC_conversion_complete == 0){}; //wait while first adc conversion is ongoing
 
+	//START FREQ. GEN and PWM GEN TIMERS and ENABLE PWM OUTPUT
 	__HAL_TIM_SET_COUNTER(&htim14, 0); //set TIM14 counter to 0
 	__HAL_TIM_ENABLE(&htim14); //enable TIM14
 	__HAL_TIM_SET_COUNTER(&htim14, 0); //set TIM16 counter to 0
