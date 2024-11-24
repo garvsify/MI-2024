04e2413384c221059f755b80ba621f5fc3c2ab47
DESKTOP-L4887SG\James Garvey <jamesgarvey@me.com>
2024-11-24 16:04:21 +0000

    It builds! Can now try and remove stuff from main to make main prettier

--- g031k8/Core/Inc/TIMx_callbacks.h
+++ g031k8/Core/Inc/TIMx_callbacks.h
@@ -3,4 +3,7 @@
 
 #include "system.h"
 
+void TIM16_callback(TIM_HandleTypeDef *htim);
+void TIM17_callback(TIM_HandleTypeDef *htim);
+
 #endif /* INC_TIMX_CALLBACKS_H_ */
--- g031k8/Core/Src/main.c
+++ g031k8/Core/Src/main.c
@@ -47,5 +47,6 @@ int main(void)
   {
 
   }
+  return 1;
 }
 
