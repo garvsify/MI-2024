04be49598c3c8bc26735745f8fc8856f4d1fc241
DESKTOP-L4887SG\James Garvey <jamesgarvey@me.com>
2024-12-17 22:05:31 +0000

    Forgot to update build comment - symmetry stuff is perfect, frequencies are perfect, just this bug to fix and we're there!!!!!

--- g031k8/Core/Src/main.c
+++ g031k8/Core/Src/main.c
@@ -1,8 +1,5 @@
 /*NOTES ON THIS BUILD:
 Every works basically perfectly until the speed is decreased through half_scale, at which point you can no longer control the speed!
-
-Also literally the only issue with the symmetry with the speed above half-scale is that it gets slightly quicker when the symmetry increases (both CW and CCW equally)
-so I imagine that the shortened period needs to be increased, because it is too quick, or the lengthened period needs to be increased because it is too quick.
 */
 
 //INCLUDES
