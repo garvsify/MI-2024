66f49dbf3614a950b6f61a5c8c937392c8b6e6c8
James Garvey <jamesgarvey@me.com>
2025-08-27 14:53:42 +0100

    decided to roll back

--- g031k8/Core/Src/custom_callbacks.c
+++ g031k8/Core/Src/custom_callbacks.c
@@ -7,19 +7,19 @@ enum Validate latched = NO;
 
 void TIM16_callback(TIM_HandleTypeDef *htim)
 {
-	//HAL_GPIO_WritePin(MONITOR_GPIO_Port, MONITOR_Pin, 1);
+	HAL_GPIO_WritePin(MONITOR_GPIO_Port, MONITOR_Pin, 1);
 
 	Set_Oscillator_Values(&params);
 	Calculate_Next_Main_Oscillator_Values(&params, (enum Next_Values_Processing_Mode)REGULAR_MODE);
 	Write_Next_Main_Oscillator_Values_to_Delay_Line(&params, &delay_line);
 	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADCResultsDMA, (uint32_t)num_ADC_conversions); //this function takes ages to execute!
 
-	//HAL_GPIO_WritePin(MONITOR_GPIO_Port, MONITOR_Pin, 0);
+	HAL_GPIO_WritePin(MONITOR_GPIO_Port, MONITOR_Pin, 0);
 }
 
 void ADC_DMA_conversion_complete_callback(ADC_HandleTypeDef *hadc)
 {
-	//HAL_GPIO_WritePin(MONITOR_GPIO_Port, MONITOR_Pin, 1);
+	HAL_GPIO_WritePin(MONITOR_2_GPIO_Port, MONITOR_2_Pin, 1);
 
 	HAL_ADC_Stop_DMA(hadc); //disable ADC DMA
 	Process_ADC_Conversion_Values(&params_manual, ADCResultsDMA);
@@ -47,7 +47,7 @@ void ADC_DMA_conversion_complete_callback(ADC_HandleTypeDef *hadc)
 		Set_Status_Bit(&statuses, Initial_ADC_Conversion_Complete);
 	}
 
-	//HAL_GPIO_WritePin(MONITOR_GPIO_Port, MONITOR_Pin, 0);
+	HAL_GPIO_WritePin(MONITOR_2_GPIO_Port, MONITOR_2_Pin, 0);
 }
 
 void TIM2_ch1_IP_capture_callback(TIM_HandleTypeDef *htim){
